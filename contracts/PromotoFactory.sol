9da73ae909a8a72d19e5880ec5ef48d23c908446
piabianca <bonillapiabianca@gmail.com>
2018-09-26 07:46:00 +0800

    modified contract and added comments

--- contracts/PromotoFactory.sol
+++ contracts/PromotoFactory.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.4.17;
 
 contract PromotoFactory {
-
+    /**
+    * This is the struct where we store our Artist information.
+    **/
     struct Artist {
         string name;
         string artistType;
@@ -11,78 +13,123 @@ contract PromotoFactory {
         mapping(address => Subscriber) subscribers;
         address[] subscribersAddress;
     }
-
+    /**
+    * This is the struct where we store our Subscriber information.
+    **/
     struct Subscriber {
         address subscriberAdd;
         uint subscribedTime;
         bool hasSubcribed;
     }
-    
+    /**
+    * We store all available artists for subscription as address => Artist
+    **/
     mapping(address => Artist) private artists;
+    /**
+    * We store all balances of the artists as address => balance
+    **/
     mapping(address => uint) private balances;
+    /**
+    * We store all artists that a user have subscribed as sender address => array of artists address
+    **/
     mapping(address => address[]) private subscribedArtists; //list of artists that the user subscribed
-    
-    
+    /**
+    * We store here the all registered artists
+    **/
     address[] private artistsList;
-
+    /**
+    * Modifier used to check if the Subscriber has a value to pass so we don't
+    * execute useless logic that has a 0 value to pass.
+    **/
     modifier hasValue {
         require(msg.value > 0);
         _;
     }
-    
+    /**
+    * Modifier used to check if the Artist has a balance in their account before cash out 
+    * so we don't execute useless logic that has a 0 balance to cash out.
+    **/    
     modifier hasBalance {
         require(balances[msg.sender] > 0);
         _;
     }
-    
-    modifier isOwner {
-        require(artists[msg.sender].artistAdd == msg.sender);
-        _;
-    }
-    
+    /**
+    * Modifier used to determinate if the Artist is the sender of the action.
+    **/
     modifier isArtist {
+        require(artists[msg.sender].artistAdd == msg.sender);
         require(artists[msg.sender].isValidArtist == true);
         _;
     }
-    
+    /**
+    * Modifier used to determinate if there are available artists to subscribe
+    **/
     modifier hasAvailableArtists {
         require(artistsList.length > 0);
         _;
     }
-
+    /**
+    * This is a function and its used to register as an artist.
+    * @param _name string - The name of the artist.
+    * @param _artistType string - The type of the artist.
+    * @param _description string - The description of the artist.
+    **/
     function registerArtist(string _name, string _artistType, string _description) public {
         require(artists[msg.sender].isValidArtist == false);
         artists[msg.sender] = Artist(_name, _artistType, _description, true, msg.sender, new address[](0));
         artistsList.push(msg.sender);
     }
-
-    function getNumberOfArtists() public view returns(uint) {
-        return artistsList.length;
+    /**
+    * Used to get the number of total artist     
+    **/
+    function getNumberOfArtists() public view returns(uint artistsCount) {
+        artistsCount = artistsList.length;
+        return artistsCount;
     } 
-
-    function pay(address _artistAdd) payable hasValue public returns (string) {
+    /**
+    * Used to pay for subscription of the artist
+    * @param _artistAdd address - Address of the artist that the user will subscribe
+    * Modifier hasValue - We determine if the subscriber is passing a value that is greater than zero
+    * Returns string - Returns the string that is equal to the value that the user has pay
+    **/
+    function pay(address _artistAdd) payable hasValue public returns (string tier) {
         balances[_artistAdd] += msg.value; 
         subscribeToArtist(_artistAdd);
         if (msg.value == 0.1 ether) {
-            return "tier 1";
+            tier = "tier 1";
         } else if (msg.value == 0.3 ether) {
-            return "tier 2";
+            tier = "tier 2";
         } else if (msg.value == 0.5 ether) {
-            return "tier 3";
-        } 
+            tier = "tier 3";
+        }
+        return tier;
     }
-
-    function cashOut() public hasBalance isOwner returns (uint) {
+    /** 
+    * Used to cash out the balance of the artist
+    * Modifier hasBalance - We determine if the susbcriber has balance in their accounts before they can cash out
+    * Modifier isArtist - We check if the User is a valid Artist
+    * Returns uint - Returns the remaining balance after cash out
+    **/
+    function cashOut() public hasBalance isArtist returns (uint remainingBalance) {
         uint amountToWithdraw = balances[msg.sender];
         msg.sender.transfer(amountToWithdraw);
-        balances[msg.sender] = 0;
-        return (balances[msg.sender]);
+        remainingBalance = balances[msg.sender] = 0;
+        return remainingBalance;
     }
-    
-    function checkBalance() public isOwner view returns (uint) {
-        return balances[msg.sender];
+    /**
+    * Used to check the balance of the artist
+    * Modifier isArtist - We check if the user is a valid Artist 
+    * Returns uint - Returns the balance of the artist
+    **/
+    function checkBalance() public isArtist view returns (uint artistBalance) {
+        artistBalance = balances[msg.sender];
+        return artistBalance;
     }
-
+    /**
+    * Used to subscribe to the artist 
+    * @param _artistAdd address - Address of the artist that the user will subscribe
+    * Modifier hasAvailableArtists - We check if there are available artists that the users can subscribe
+    **/
     function subscribeToArtist(address _artistAdd) hasAvailableArtists public {
         require(artists[_artistAdd].isValidArtist == true);
         require(now > artists[_artistAdd].subscribers[msg.sender].subscribedTime + 6 days);  
@@ -94,9 +141,13 @@ contract PromotoFactory {
         }
         artists[_artistAdd].subscribers[msg.sender].subscribedTime = now + 6 days;
     }
-    
-    function getSubscribersCount() view public isArtist isOwner returns (uint) {
-        uint count = 0;
+    /**
+    * Used to get the subscribers count of each artist
+    * Modifier isArtist - We check if the user is a valid Artist
+    * Returns uint - Returns the count of the subscribers
+    **/
+    function getSubscribersCount() view public isArtist returns (uint count) {
+        count = 0;
         for (uint index = 0; index < artists[msg.sender].subscribersAddress.length; index++) {
             address subs = artists[msg.sender].subscribersAddress[index];
             if (artists[msg.sender].subscribers[subs].subscribedTime > now) {
@@ -104,9 +155,5 @@ contract PromotoFactory {
             }
         }
         return count;
-    }
-    
-    function getSubcriberTime(address _subsAdd) public view isArtist returns (uint) {
-        return (artists[msg.sender].subscribers[_subsAdd].subscribedTime);
     }
 }
